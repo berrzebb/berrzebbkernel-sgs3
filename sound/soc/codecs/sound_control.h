55c4779db1d4e759f249622723d97f0131447c30
berrzebb <ph4913@gmail.com>
2012-11-07 20:22:08 +0900

    Revert "Sound Control V2: add proper sysfs interface for volume_boost. HUGE props to Supercurio as this is mostly based on his fabulous work."
    This reverts commit b833db71ee3404e582bb674ac6699b0ee121510e.

--- sound/soc/codecs/sound_control.h
+++
@@ -1,3 +0,0 @@
-void update_volume(void);
-unsigned int sound_control_wm8994_write(struct snd_soc_codec *codec, unsigned int reg, unsigned int value);
-void sound_control_wm8994_pcm_probe(struct snd_soc_codec *codec);
