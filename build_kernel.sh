e872451edc51b06faa3d0b619719bdc51a7f0014
berrzebb <ph4913@gmail.com>
2012-11-07 21:18:30 +0900

    Wakelock and Cgroup Patch

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,48 @@
-#!/bin/bash
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+export RAMFS_SOURCE=`readlink -f $KERNELDIR/ramfs_$1`
+export PARENT_DIR=`readlink -f ..`
+export USE_SEC_FIPS_MODE=true
+export DROPBOX_DIR=`readlink -f ../../Dropbox/`
+export CROSS_COMPILE=$PARENT_DIR/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+
+RAMFS_TMP="/tmp/ramfs-source"
 
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.4.3/bin/arm-eabi-
 
-make c1ktt_00_defconfig
-make
+cd $KERNELDIR/
+nice -n 10 make -j4 -ofast USE_CCACHE=1 || exit 1
+
+#remove previous ramfs files
+rm -rf $RAMFS_TMP
+rm -rf $RAMFS_TMP.cpio
+rm -rf $RAMFS_TMP.cpio.gz
+#copy ramfs files to tmp directory
+cp -ax $RAMFS_SOURCE $RAMFS_TMP
+#clear git repositories in ramfs
+find $RAMFS_TMP -name .git -exec rm -rf {} \;
+#remove empty directory placeholders
+find $RAMFS_TMP -name EMPTY_DIRECTORY -exec rm -rf {} \;
+rm -rf $RAMFS_TMP/tmp/*
+#remove mercurial repository
+rm -rf $RAMFS_TMP/.hg
+#copy modules into ramfs
+mkdir -p $RAMFS_TMP/lib/modules
+find -name '*.ko' -exec cp -av {} $RAMFS_TMP/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded $RAMFS_TMP/lib/modules/*
+
+cd $RAMFS_TMP
+find | fakeroot cpio -H newc -o > $RAMFS_TMP.cpio 2>/dev/null
+ls -lh $RAMFS_TMP.cpio
+gzip -9 $RAMFS_TMP.cpio
+cd -
+
+nice -n 10 make -j3 USE_CCACHE=1 zImage || exit 1
+
+./mkbootimg --kernel $KERNELDIR/arch/arm/boot/zImage --ramdisk $RAMFS_TMP.cpio.gz --board smdk4x12 --base 0x10000000 --pagesize 2048 --ramdiskaddr 0x11000000 -o $KERNELDIR/boot.img.pre
+
+$KERNELDIR/mkshbootimg.py $KERNELDIR/boot.img $KERNELDIR/boot.img.pre $KERNELDIR/payload.tar
+rm -f $KERNELDIR/boot.img.pre
+rm -f $DROPBOXDIR/boot.tar
+rm -f $KERNELDIR/boot.tar
+tar cvf boot.tar boot.img
