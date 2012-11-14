16f8795c864b8df9da1b871790f8d0d4731ad9ae
berrzebb <ph4913@gmail.com>
2012-11-14 19:35:57 +0900

    Next

--- drivers/net/ibmveth.c
+++ drivers/net/ibmveth.c
@@ -756,7 +756,7 @@ static int ibmveth_set_csum_offload(struct net_device *dev, u32 data)
 	struct ibmveth_adapter *adapter = netdev_priv(dev);
 	unsigned long set_attr, clr_attr, ret_attr;
 	unsigned long set_attr6, clr_attr6;
-	long ret, ret4, ret6;
+	long ret, ret6;
 	int rc1 = 0, rc2 = 0;
 	int restart = 0;
 
@@ -769,8 +769,6 @@ static int ibmveth_set_csum_offload(struct net_device *dev, u32 data)
 
 	set_attr = 0;
 	clr_attr = 0;
-	set_attr6 = 0;
-	clr_attr6 = 0;
 
 	if (data) {
 		set_attr = IBMVETH_ILLAN_IPV4_TCP_CSUM;
@@ -785,20 +783,16 @@ static int ibmveth_set_csum_offload(struct net_device *dev, u32 data)
 	if (ret == H_SUCCESS && !(ret_attr & IBMVETH_ILLAN_ACTIVE_TRUNK) &&
 	    !(ret_attr & IBMVETH_ILLAN_TRUNK_PRI_MASK) &&
 	    (ret_attr & IBMVETH_ILLAN_PADDED_PKT_CSUM)) {
-		ret4 = h_illan_attributes(adapter->vdev->unit_address, clr_attr,
+		ret = h_illan_attributes(adapter->vdev->unit_address, clr_attr,
 					 set_attr, &ret_attr);
 
-		if (ret4 != H_SUCCESS) {
+		if (ret != H_SUCCESS) {
 			netdev_err(dev, "unable to change IPv4 checksum "
 					"offload settings. %d rc=%ld\n",
-					data, ret4);
+					data, ret);
 
-			h_illan_attributes(adapter->vdev->unit_address,
-					   set_attr, clr_attr, &ret_attr);
-
-			if (data == 1)
-				dev->features &= ~NETIF_F_IP_CSUM;
-
+			ret = h_illan_attributes(adapter->vdev->unit_address,
+						 set_attr, clr_attr, &ret_attr);
 		} else {
 			adapter->fw_ipv4_csum_support = data;
 		}
@@ -809,18 +803,15 @@ static int ibmveth_set_csum_offload(struct net_device *dev, u32 data)
 		if (ret6 != H_SUCCESS) {
 			netdev_err(dev, "unable to change IPv6 checksum "
 					"offload settings. %d rc=%ld\n",
-					data, ret6);
+					data, ret);
 
-			h_illan_attributes(adapter->vdev->unit_address,
-					   set_attr6, clr_attr6, &ret_attr);
-
-			if (data == 1)
-				dev->features &= ~NETIF_F_IPV6_CSUM;
-
+			ret = h_illan_attributes(adapter->vdev->unit_address,
+						 set_attr6, clr_attr6,
+						 &ret_attr);
 		} else
 			adapter->fw_ipv6_csum_support = data;
 
-		if (ret4 == H_SUCCESS || ret6 == H_SUCCESS)
+		if (ret == H_SUCCESS || ret6 == H_SUCCESS)
 			adapter->rx_csum = data;
 		else
 			rc1 = -EIO;
